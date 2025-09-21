a568da289fe24e2e7b07494283e0f6f642b51f77
Obi <obi6745@gmail.com>
2025-09-21 19:49:14 -0400

    Add project files.

---
+++ NewsService/Models/Article.cs
@@ -0,0 +1,22 @@
+﻿using System; 
+
+namespace NewsService.Models
+{
+    public class Article
+    {
+        public int ArticleId { get; set; }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+
+        public int CategoryId { get; set; }
+
+
+        public Category Category { get; set; }
+
+
+
+    }
+}
---
+++ NewsService/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace NewsService.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; } // this would be the primary key 
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<Article> Articles { get; set; }
+    }
+}
